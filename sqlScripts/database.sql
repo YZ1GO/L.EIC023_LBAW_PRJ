584e2c683884fa6a5bd437698644230530083f5c
Francisco Magalhães <your.email@example.com>
2024-10-31 19:26:00 +0000

    Adding on update cascades to sql database script.

--- sqlScripts/database.sql
+++ sqlScripts/database.sql
@@ -55,14 +55,14 @@ CREATE TABLE Administrator(
 );
 
 CREATE TABLE Buyer (
-    id INT PRIMARY KEY REFERENCES Users(id),
+    id INT PRIMARY KEY REFERENCES Users(id) ON UPDATE CASCADE,
     NIF TEXT UNIQUE,
     birth_date DATE NOT NULL CHECK(birth_date <= CURRENT_DATE),
     coins INT NOT NULL CHECK(coins >= 0) DEFAULT 0
 );
 
 CREATE TABLE Seller(
-    id INT PRIMARY KEY REFERENCES Users(id)
+    id INT PRIMARY KEY REFERENCES Users(id) ON UPDATE CASCADE
 );
 
 CREATE TABLE Game(
@@ -72,14 +72,14 @@ CREATE TABLE Game(
     minimum_age INT NOT NULL CHECK(minimum_age >= 0 AND minimum_age <= 18),
     price FLOAT NOT NULL CHECK(price >= 0.0),
     overall_rating INT NOT NULL CHECK(overall_rating >= 0 AND overall_rating <= 100),
-    owner INT NOT NULL REFERENCES Seller(id),
+    owner INT NOT NULL REFERENCES Seller(id) ON UPDATE CASCADE,
     is_active BOOLEAN DEFAULT TRUE
 );
 
 CREATE TABLE CDK(
     id SERIAL PRIMARY KEY,
     code TEXT UNIQUE NOT NULL,
-    game INT NOT NULL REFERENCES Game(id)
+    game INT NOT NULL REFERENCES Game(id) ON UPDATE CASCADE
 );
 
 CREATE TABLE Platform(
@@ -104,49 +104,49 @@ CREATE TABLE Player(
 
 CREATE TABLE GamePlatform(
     id SERIAL PRIMARY KEY,
-    game INT NOT NULL REFERENCES Game(id),
-    platform INT NOT NULL REFERENCES Platform(id),
+    game INT NOT NULL REFERENCES Game(id) ON UPDATE CASCADE,
+    platform INT NOT NULL REFERENCES Platform(id) ON UPDATE CASCADE,
     CONSTRAINT game_platform_pair_unique UNIQUE (game,platform)
 );
 
 CREATE TABLE GameCategory(
     id SERIAL PRIMARY KEY,
-    game INT NOT NULL REFERENCES Game(id),
-    category INT NOT NULL REFERENCES Category(id),
+    game INT NOT NULL REFERENCES Game(id) ON UPDATE CASCADE,
+    category INT NOT NULL REFERENCES Category(id) ON UPDATE CASCADE,
     CONSTRAINT game_category_pair_unique UNIQUE (game,category)
 );
 
 CREATE TABLE GameLanguage(
     id SERIAL PRIMARY KEY,
-    game INT NOT NULL REFERENCES Game(id),
-    language INT NOT NULL REFERENCES Language(id),
+    game INT NOT NULL REFERENCES Game(id) ON UPDATE CASCADE,
+    language INT NOT NULL REFERENCES Language(id) ON UPDATE CASCADE,
     CONSTRAINT game_language_pair_unique UNIQUE (game,language)
 );
 
 CREATE TABLE GamePlayer(
     id SERIAL PRIMARY KEY,
-    game INT NOT NULL REFERENCES Game(id),
-    player INT NOT NULL REFERENCES Player(id),
+    game INT NOT NULL REFERENCES Game(id) ON UPDATE CASCADE,
+    player INT NOT NULL REFERENCES Player(id) ON UPDATE CASCADE,
     CONSTRAINT game_player_pair_unique UNIQUE (game,player)
 );
 
 CREATE TABLE Media(
     id SERIAL PRIMARY KEY,
     path TEXT NOT NULL,
-    game INT NOT NULL REFERENCES Game(id)
+    game INT NOT NULL REFERENCES Game(id) ON UPDATE CASCADE
 );
 
 CREATE TABLE Wishlist(
     id SERIAL PRIMARY KEY,
-    buyer INT NOT NULL REFERENCES Buyer(id),
-    game INT NOT NULL REFERENCES Game(id),
+    buyer INT NOT NULL REFERENCES Buyer(id) ON UPDATE CASCADE,
+    game INT NOT NULL REFERENCES Game(id) ON UPDATE CASCADE,
     CONSTRAINT buyer_game_pair_unique UNIQUE (buyer, game)
 );
 
 CREATE TABLE ShoppingCart(
     id SERIAL PRIMARY KEY,
-    buyer INT NOT NULL REFERENCES Buyer(id),
-    game INT NOT NULL REFERENCES Game(id),
+    buyer INT NOT NULL REFERENCES Buyer(id) ON UPDATE CASCADE,
+    game INT NOT NULL REFERENCES Game(id) ON UPDATE CASCADE,
     CONSTRAINT buyer_game_pair_2_unique UNIQUE (buyer, game)
 );
 
@@ -157,14 +157,14 @@ CREATE TABLE PaymentMethod(
 
 CREATE TABLE Payment(
     id SERIAL PRIMARY KEY,
-    method INT NOT NULL REFERENCES PaymentMethod(id),
+    method INT NOT NULL REFERENCES PaymentMethod(id) ON UPDATE CASCADE,
     value FLOAT NOT NULL CHECK(value >= 0.0)
 );
 
 CREATE TABLE Orders(
     id SERIAL PRIMARY KEY,
-    buyer INT NOT NULL REFERENCES Buyer(id),
-    payment INT NOT NULL UNIQUE REFERENCES Payment(id),
+    buyer INT NOT NULL REFERENCES Buyer(id) ON UPDATE CASCADE,
+    payment INT NOT NULL UNIQUE REFERENCES Payment(id) ON UPDATE CASCADE,
     time TIMESTAMP NOT NULL CHECK (time <= CURRENT_TIMESTAMP) DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT buyer_time_pair_unique UNIQUE (buyer, time)
 );
@@ -172,22 +172,22 @@ CREATE TABLE Orders(
 CREATE TABLE Purchase(
     id SERIAL PRIMARY KEY,
     value FLOAT NOT NULL CHECK(value >= 0.0),
-    order_ INT NOT NULL REFERENCES Orders(id)
+    order_ INT NOT NULL REFERENCES Orders(id) ON UPDATE CASCADE
 );
 
 CREATE TABLE PrePurchase(
-    id INT PRIMARY KEY REFERENCES Purchase(id),
-    game INT NOT NULL REFERENCES Game(id)
+    id INT PRIMARY KEY REFERENCES Purchase(id) ON UPDATE CASCADE,
+    game INT NOT NULL REFERENCES Game(id) ON UPDATE CASCADE
 );
 
 CREATE TABLE CanceledPurchase(
-    id INT PRIMARY KEY REFERENCES Purchase(id),
-    game INT NOT NULL REFERENCES Game(id)
+    id INT PRIMARY KEY REFERENCES Purchase(id) ON UPDATE CASCADE,
+    game INT NOT NULL REFERENCES Game(id) ON UPDATE CASCADE
 );
 
 CREATE TABLE DeliveredPurchase(
-    id INT PRIMARY KEY REFERENCES Purchase(id),
-    cdk INT NOT NULL REFERENCES CDK(id)
+    id INT PRIMARY KEY REFERENCES Purchase(id) ON UPDATE CASCADE,
+    cdk INT UNIQUE NOT NULL REFERENCES CDK(id) ON UPDATE CASCADE
 );
 
 CREATE TABLE Review(
@@ -195,15 +195,15 @@ CREATE TABLE Review(
     title TEXT NOT NULL,
     description TEXT NOT NULL,
     positive BOOLEAN NOT NULL,
-    author INT NOT NULL REFERENCES Buyer(id),
-    game INT NOT NULL REFERENCES Game(id),
+    author INT NOT NULL REFERENCES Buyer(id) ON UPDATE CASCADE,
+    game INT NOT NULL REFERENCES Game(id) ON UPDATE CASCADE,
     CONSTRAINT author_game_pair_unique UNIQUE (author, game)
 );
 
 CREATE TABLE ReviewLike(
     id SERIAL PRIMARY KEY,
-    review INT NOT NULL REFERENCES Review(id),
-    author INT NOT NULL REFERENCES Buyer(id),
+    review INT NOT NULL REFERENCES Review(id) ON UPDATE CASCADE,
+    author INT NOT NULL REFERENCES Buyer(id) ON UPDATE CASCADE,
     CONSTRAINT review_author_pair_unique UNIQUE (review, author)
 );
 
@@ -214,29 +214,29 @@ CREATE TABLE Notifications(
 );
 
 CREATE TABLE NotificationWishlist(
-    id INT PRIMARY KEY REFERENCES Notifications(id),
-    wishlist INT NOT NULL REFERENCES Wishlist(id)
+    id INT PRIMARY KEY REFERENCES Notifications(id) ON UPDATE CASCADE,
+    wishlist INT NOT NULL REFERENCES Wishlist(id) ON UPDATE CASCADE
 );
 
 CREATE TABLE NotificationGame(
-    id INT PRIMARY KEY REFERENCES Notifications(id),
-    game INT NOT NULL REFERENCES Game(id)
+    id INT PRIMARY KEY REFERENCES Notifications(id) ON UPDATE CASCADE,
+    game INT NOT NULL REFERENCES Game(id) ON UPDATE CASCADE
 );
 
 CREATE TABLE NotificationPurchase(
-    id INT PRIMARY KEY REFERENCES Notifications(id),
-    purchase INT NOT NULL REFERENCES Purchase(id)
+    id INT PRIMARY KEY REFERENCES Notifications(id) ON UPDATE CASCADE,
+    purchase INT NOT NULL REFERENCES Purchase(id) ON UPDATE CASCADE
 );
 
 CREATE TABLE NotificationReview(
-    id INT PRIMARY KEY REFERENCES Notifications(id),
-    review INT NOT NULL REFERENCES Review(id)
+    id INT PRIMARY KEY REFERENCES Notifications(id) ON UPDATE CASCADE,
+    review INT NOT NULL REFERENCES Review(id) ON UPDATE CASCADE
 );
 
 CREATE TABLE UserNotifications(
     id SERIAL PRIMARY KEY,
-    notification INT REFERENCES Notifications(id),
-    receiver INT REFERENCES Users(id),
+    notification INT REFERENCES Notifications(id) ON UPDATE CASCADE,
+    receiver INT REFERENCES Users(id) ON UPDATE CASCADE,
     time TIMESTAMP NOT NULL CHECK (time <= CURRENT_TIMESTAMP) DEFAULT CURRENT_TIMESTAMP,
     isRead BOOLEAN NOT NULL DEFAULT FALSE
 );
@@ -248,9 +248,9 @@ CREATE TABLE Reason(
 
 CREATE TABLE Report(
     id SERIAL PRIMARY KEY,
-    buyer INT NOT NULL REFERENCES Buyer(id),
-    review INT NOT NULL REFERENCES Review(id),
-    reason INT REFERENCES Reason(id),
+    buyer INT NOT NULL REFERENCES Buyer(id) ON UPDATE CASCADE,
+    review INT NOT NULL REFERENCES Review(id) ON UPDATE CASCADE,
+    reason INT REFERENCES Reason(id) ON UPDATE CASCADE,
     description TEXT,
     CONSTRAINT reason_or_description_not_null CHECK (reason IS NOT NULL OR description IS NOT NULL)
 );
@@ -295,4 +295,5 @@ dados de cartão e users e depois até reformular a lógoca de pagamento à volt
 são reais.
 
 */
+
 
